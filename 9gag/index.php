47c94a5dd24a90be7bf5a4475692b04010e971ab
Kerem Tatlici <krmttlc@gmail.com>
2018-03-19 01:27:34 +0300

    .

---
+++ 9gag/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>9GAG: Go Fun The World</title>
+		<link href="https://assets-9gag-fun.9cache.com/s/fab0aa49/2c4d7fa71528a5c87256c0d1c2c9165d11fe2d4c/static/dist/web6/css/gag.css"  rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header id="top-nav">
+            <div class="nav-wrap">
+				<a class="logo" href="index.php">9GAG</a>
+				<nav class="nav-menu">
+					<ul class="secondary">
+						<li class="outer">
+							<a href="https://9gag.com/tv?ref=9nav">TV</a>
+						</li>
+						<li class="outer">
+							<a href="https://9gag.com/video?ref=nav">Video 📺</a>
+						</li>
+						<li class="outer">
+							<a href="https://9gag.com/mobile?ref=9nav">Get Our App!</a>
+						</li>
+						<li class="outer">
+							<a href="https://notsafeforwork.com?ref=9nav">NSFW Clothing - Sale</a>
+						</li>
+					</ul>
+				</nav>   
+				<div class="function-wrap">
+					<div class="general-function">
+						<a class="search" href="#">Search</a>
+					</div>
+					<div class="visitor-function">
+						<a class="btn-mute" href="https://9gag.com/login">Log in</a>
+						<a class="btn-primary" href="https://9gag.com/signup" >Sign up</a>
+					</div>
+				</div>	
+			</div>
+		</header>			
+    </body>
+</html>
